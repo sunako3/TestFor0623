504abb1264cb01e0890d49c3107561bc792a5319
Sunako <konasuhikoyuki@gmail.com>
2015-06-23 15:06:57 +0900

    Initial commit

---
+++ app/src/androidTest/java/jp/androidbook/myapp/testfor0623/ApplicationTest.java
@@ -0,0 +1,13 @@
+package jp.androidbook.myapp.testfor0623;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
